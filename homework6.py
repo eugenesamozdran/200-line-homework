c82cd7628ce1baaedbb1102ab2ec8ba9e06c9e2f
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-11-20 00:49:30 +0200

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -2,6 +2,7 @@ import string
 
 def count_holes(n):
 
+    # defining the number of 'holes' for each digit string representation
     HOLE_DICT = {
                     "0": 1,
                     "1": 0,
@@ -16,16 +17,19 @@ def count_holes(n):
                     "-": 0
                 }
 
-    hole_number = 0
+    # allowed symbols is required for checking if any of argument characters do not match integer number pattern
     ALLOWED_SYMBOLS = tuple("-" + string.digits)
+    hole_number = 0
     
+    # printing error message if the argument does not look like the number at all (or contains characters different from digits or '-')
+    # and counting number of 'holes'
     for char in str(n):
         if char not in ALLOWED_SYMBOLS:
             print("ERROR")
             break
     else:
-        for char in str(int(n)):
+        # the argument is converted to integer and back to string in order to get rid of unnecessary zeroes at the beginning (if any)
+        for char in str(int(n)): 
             hole_number += HOLE_DICT[char]
         print("The number of holes is {}".format(hole_number))
        
-count_holes("-168")
