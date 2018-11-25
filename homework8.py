03c1a830b259c9d3ad29df45e4d489ac891f9b81
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-11-25 20:36:37 +0200

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,25 @@
+def bubble_sort(iterable, key=None, reverse=False):
+
+    if type(iterable).__name__ in ("str", "tuple", "set", "dict"):
+        iterable = list(iterable)
+
+    if key == None:
+        pass
+    else:
+        iterable = list(map(key, iterable))
+
+    for i in range(len(iterable)-1):
+            if iterable[i] > iterable[i+1]:
+                iterable[i], iterable[i+1] = iterable[i+1], iterable[i]
+                bubble_sort(iterable)
+                
+    if reverse == False:
+        return iterable
+    else:
+        return iterable[::-1]
+
+a = {"zekrjgekrg": 3, "cgegerg": 3, "gr": 2}
+
+a = bubble_sort(a, len)
+
+print(a)
