54b38318ca66b49f58e772485bc7a2639aa39ef7
eugenesamozdran <44786116+eugenesamozdran@users.noreply.github.com>
2018-11-26 23:41:10 +0200

    Add files via upload

---
+++ builtin_func_examples.py
@@ -0,0 +1,19 @@
+from itertools import dropwhile
+
+lst = list(range(0, 10))
+
+a = list(map(lambda x: x**2, lst))
+print("map func for 'lst':", a)
+
+b = list(filter(lambda x: x%2, lst))
+print("filter func for 'lst':", b)
+
+d = list(dropwhile(lambda x: x<4, lst))
+print("printing 'lst' elements as soon as any of them dont't meet the condition:", d)
+
+y = 4
+z = 4
+c = [(y is z), (z is y), (y != 4), (z == 4)]
+print("are all the elements of 'c' true? -", all(c))
+
+print("are any of the elements of 'c' true? -", any(c))
